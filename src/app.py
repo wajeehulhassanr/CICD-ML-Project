588463164126ce546ffcaee19ac9e415c924a445
Moeez Khan Yousafzai <i211694@nu.edu.pk>
2025-03-06 17:10:06 +0500

    Added a basic HTML file for the Python app

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import os
 import joblib
 import logging
+from flask import render_template
 from flask import Flask, request, jsonify
 import numpy as np
 import pandas as pd
@@ -153,7 +154,14 @@ def model_info():
             'status': 'error',
             'message': str(e)
         }), 500
+        
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
+
+
